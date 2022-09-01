d0aed6ab5f2d5c9f79523aebd55c1d4e6fb86320
rizalyala <rizalyala19@gmail.com>
2022-09-01 12:37:27 +0700

    oke

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from 'cors';
+import { PORT } from "./config/constants";
+import { mhsRouter } from "./routes";
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.get("/",(req,res)=>{
+    res.send("Selamat datang di RESTful API gateway SI Akademik")
+})
+app.use("/mhs",mhsRouter)
+
+app.listen(PORT,()=>{
+    console.log(`Endpoint Akademik sudah siap dan bisa diakses di port ${PORT}`)
+})
+
